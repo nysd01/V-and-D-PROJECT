df9affcd69e1102c906db269d2c41e6b6e1ddb63
NYSD DEREK <124192421+nysd01@users.noreply.github.com>
2026-05-30 16:13:58 +0100

    Fix: add babel.config.js with module-resolver for @/ alias in Docker

--- frontend/InternConnect/metro.config.js
+++ frontend/InternConnect/metro.config.js
@@ -1,12 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('glb', 'gltf', 'usdz');
-
-config.resolver.extraNodeModules = {
-  '@': path.resolve(__dirname),
-};
 
 module.exports = config;
