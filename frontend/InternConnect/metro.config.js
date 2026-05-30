d57f8f89bbb3a56f21eb584a617a3f06389802c3
NYSD DEREK <124192421+nysd01@users.noreply.github.com>
2026-05-30 16:04:31 +0100

    Fix: add @/ alias to metro config for Docker web build

--- frontend/InternConnect/metro.config.js
+++ frontend/InternConnect/metro.config.js
@@ -1,7 +1,12 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('glb', 'gltf', 'usdz');
+
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname),
+};
 
 module.exports = config;
